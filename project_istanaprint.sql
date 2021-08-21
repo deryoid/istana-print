9a391f8b6602e13034939f23d6e274e1f53fd327
G-MF <gusti.fahrubi@gmail.com>
2021-08-21 20:37:56 +0800

    export new database

--- project_istanaprint.sql
+++ project_istanaprint.sql
@@ -1,214 +1,150 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Waktu pembuatan: 12 Agu 2021 pada 09.25
--- Versi server: 5.7.24
--- Versi PHP: 7.4.12
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+ Source Server         : Connection MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 50724
+ Source Host           : localhost:3306
+ Source Schema         : project_istanaprint
 
+ Target Server Type    : MySQL
+ Target Server Version : 50724
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 21/08/2021 20:36:44
+*/
 
---
--- Database: `project_istanaprint`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for karyawan
+-- ----------------------------
+DROP TABLE IF EXISTS `karyawan`;
+CREATE TABLE `karyawan`  (
+  `id_karyawan` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_karyawan` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `jk` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `tempat_lahir` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `tgl_lahir` date NULL DEFAULT NULL,
+  `agama` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `pendidikan` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `jurusan` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `alamat` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  `hp` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `bidang` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `jabatan` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_karyawan`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
---
--- Struktur dari tabel `karyawan`
---
+-- ----------------------------
+-- Records of karyawan
+-- ----------------------------
+INSERT INTO `karyawan` VALUES (1, 'Alfianoor', 'Laki-laki', 'Banjarmasin', '1990-12-31', 'Islam', 'S1', 'TI', 'Banjarmasin', '0979868756755', 'Design', 'ASDAFA');
+INSERT INTO `karyawan` VALUES (2, 'Bana', 'Laki-laki', 'Banjarmasin', '1980-12-31', 'Islam', 'D3', 'MI', 'Banjarmasin', '09786543567867', 'Design', 'adfasf');
+INSERT INTO `karyawan` VALUES (3, 'Gusti', 'Laki-laki', 'Berangas', '1996-04-14', 'Islam', 'S1', 'TI', 'Jl. Berangas Rt. 06 Rw. 02', '087815357404', 'Design', 'Staff');
 
-CREATE TABLE `karyawan` (
-  `id_karyawan` int(11) NOT NULL,
-  `nama_karyawan` varchar(150) NOT NULL,
-  `bidang` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- ----------------------------
+-- Table structure for katalog
+-- ----------------------------
+DROP TABLE IF EXISTS `katalog`;
+CREATE TABLE `katalog`  (
+  `id_katalog` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_katalog` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `jenis_katalog` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `qty` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `ukuran` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `harga` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `harga_desain` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `total_harga` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `file` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_katalog`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data untuk tabel `karyawan`
---
+-- ----------------------------
+-- Records of katalog
+-- ----------------------------
+INSERT INTO `katalog` VALUES (1, 'X Banner', 'X Banner', '15', '10 x 25 ', '90000', '20000', '110000', '18869.jpg');
+INSERT INTO `katalog` VALUES (2, 'Spanduk', 'Spanduk', '6', '300 x 100', '90000', '20000', '11000', '9059.jpg');
+INSERT INTO `katalog` VALUES (3, 'ID Card', 'ID Card', '2000', '50 x 40', '1500', '20000', '21500', '72706.jpg');
+INSERT INTO `katalog` VALUES (4, 'Pamflet', 'Pamflet', '1000', '30 x 30', '5000', '20000', '25000', '89243.jpg');
+INSERT INTO `katalog` VALUES (8, 'qwerty', 'asddasf', '12', 'dsafasf', '121212', '1212', '1212121', '93867.jpg');
 
-INSERT INTO `karyawan` (`id_karyawan`, `nama_karyawan`, `bidang`) VALUES
-(1, 'Alfianoor', 'Design'),
-(2, 'Bana', 'Design');
+-- ----------------------------
+-- Table structure for pelanggan
+-- ----------------------------
+DROP TABLE IF EXISTS `pelanggan`;
+CREATE TABLE `pelanggan`  (
+  `id_pelanggan` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `nama_pelanggan` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `jk` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `alamat` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  `no_hp` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_pelanggan`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of pelanggan
+-- ----------------------------
+INSERT INTO `pelanggan` VALUES ('PLG001', 'Fawazzz', 'Laki-laki', 'Banjarmasin', '769868596769');
+INSERT INTO `pelanggan` VALUES ('PLG002', 'Dery', 'Laki-laki', 'Komplek Smanda 2 Banjarmasin', '092957459879');
 
---
--- Struktur dari tabel `katalog`
---
+-- ----------------------------
+-- Table structure for pemesanan
+-- ----------------------------
+DROP TABLE IF EXISTS `pemesanan`;
+CREATE TABLE `pemesanan`  (
+  `id_pemesanan` int(11) NOT NULL AUTO_INCREMENT,
+  `tanggal_pesan` date NULL DEFAULT NULL,
+  `id_pelanggan` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `id_katalog` int(11) NULL DEFAULT NULL,
+  `file` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `tipe_pembayaran` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `status_bayar` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `status_pengerjaan` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `status_pengambilan` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_pemesanan`) USING BTREE,
+  INDEX `id_katalog`(`id_katalog`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `katalog` (
-  `id_katalog` int(11) NOT NULL,
-  `nama_katalog` varchar(150) NOT NULL,
-  `jenis_katalog` varchar(150) NOT NULL,
-  `qty` varchar(50) NOT NULL,
-  `ukuran` varchar(150) NOT NULL,
-  `harga` varchar(50) NOT NULL,
-  `harga_desain` varchar(50) NOT NULL,
-  `total_harga` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- ----------------------------
+-- Records of pemesanan
+-- ----------------------------
+INSERT INTO `pemesanan` VALUES (1, '2021-08-20', 'PLG001', 1, '96093.jpg', 'Transfer', 'Sudah Dibayar', 'Selesai', 'Belum Diambil');
+INSERT INTO `pemesanan` VALUES (2, '2021-08-20', 'PLG002', 4, '95297.jpg', 'Cash', 'Sudah Dibayar', 'Selesai', 'Sudah Diambil');
 
---
--- Dumping data untuk tabel `katalog`
---
+-- ----------------------------
+-- Table structure for produk_gagal
+-- ----------------------------
+DROP TABLE IF EXISTS `produk_gagal`;
+CREATE TABLE `produk_gagal`  (
+  `id_pg` int(11) NOT NULL AUTO_INCREMENT,
+  `tanggal` date NULL DEFAULT NULL,
+  `id_pemesanan` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `keterangan` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  PRIMARY KEY (`id_pg`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
-INSERT INTO `katalog` (`id_katalog`, `nama_katalog`, `jenis_katalog`, `qty`, `ukuran`, `harga`, `harga_desain`, `total_harga`) VALUES
-(1, 'X Banner', 'X Banner', '15', '10 x 25 ', '90000', '20000', '110000'),
-(2, 'Spanduk', 'Spanduk', '6', '300 x 100', '90000', '20000', '11000'),
-(3, 'ID Card', 'ID Card', '2000', '50 x 40', '1500', '20000', '21500'),
-(4, 'Pamflet', 'Pamflet', '1000', '30 x 30', '5000', '20000', '25000');
+-- ----------------------------
+-- Records of produk_gagal
+-- ----------------------------
+INSERT INTO `produk_gagal` VALUES (2, '2021-08-21', '1', 'hasil cetak rusak akan dicetak ulang');
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `role` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id_user`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
---
--- Struktur dari tabel `pemesanan`
---
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Super Admin');
 
-CREATE TABLE `pemesanan` (
-  `id_pemesanan` int(11) NOT NULL,
-  `nama_pemesan` varchar(255) DEFAULT NULL,
-  `nik` varchar(255) DEFAULT NULL,
-  `no_wa` varchar(100) DEFAULT NULL,
-  `id_katalog` int(11) DEFAULT NULL,
-  `tanggal_pesan` date DEFAULT NULL,
-  `file` varchar(255) DEFAULT NULL,
-  `status` varchar(20) DEFAULT NULL,
-  `status_pengambilan` varchar(150) DEFAULT NULL,
-  `id_karyawan` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `pemesanan`
---
-
-INSERT INTO `pemesanan` (`id_pemesanan`, `nama_pemesan`, `nik`, `no_wa`, `id_katalog`, `tanggal_pesan`, `file`, `status`, `status_pengambilan`, `id_karyawan`) VALUES
-(1, 'Salabiah', '6371022210960004', '089579238888', 1, '2021-08-11', '78987.JPG', 'Selesai', 'Sudah Diambil', 1),
-(3, 'Fakhri', '637102221210008', '089579238888', 4, '2021-08-12', '32162.jpg', 'Proses', NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `pengembalian`
---
-
-CREATE TABLE `pengembalian` (
-  `id_pengembalian` int(11) NOT NULL,
-  `nama_pemesan` varchar(255) DEFAULT NULL,
-  `nik` varchar(255) DEFAULT NULL,
-  `no_wa` varchar(100) DEFAULT NULL,
-  `tanggal_kembali` date DEFAULT NULL,
-  `file` varchar(255) DEFAULT NULL,
-  `status` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `pengembalian`
---
-
-INSERT INTO `pengembalian` (`id_pengembalian`, `nama_pemesan`, `nik`, `no_wa`, `tanggal_kembali`, `file`, `status`) VALUES
-(4, 'Udin', '635233210960004', '089579238888', '2021-08-12', '21383.jpg', 'Menunggu Persetujuan');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `user`
---
-
-CREATE TABLE `user` (
-  `id_user` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `role` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `user`
---
-
-INSERT INTO `user` (`id_user`, `username`, `password`, `role`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Super Admin');
-
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `karyawan`
---
-ALTER TABLE `karyawan`
-  ADD PRIMARY KEY (`id_karyawan`);
-
---
--- Indeks untuk tabel `katalog`
---
-ALTER TABLE `katalog`
-  ADD PRIMARY KEY (`id_katalog`);
-
---
--- Indeks untuk tabel `pemesanan`
---
-ALTER TABLE `pemesanan`
-  ADD PRIMARY KEY (`id_pemesanan`),
-  ADD KEY `id_katalog` (`id_katalog`),
-  ADD KEY `id_karyawan` (`id_karyawan`);
-
---
--- Indeks untuk tabel `pengembalian`
---
-ALTER TABLE `pengembalian`
-  ADD PRIMARY KEY (`id_pengembalian`);
-
---
--- Indeks untuk tabel `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `karyawan`
---
-ALTER TABLE `karyawan`
-  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `katalog`
---
-ALTER TABLE `katalog`
-  MODIFY `id_katalog` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT untuk tabel `pemesanan`
---
-ALTER TABLE `pemesanan`
-  MODIFY `id_pemesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT untuk tabel `pengembalian`
---
-ALTER TABLE `pengembalian`
-  MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT untuk tabel `user`
---
-ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
